b3603cd391fa674bd180ba65e92b0cf72784fa4d
sakii <fukumoto.saki@keio.jp>
2026-07-19 00:24:38 +0900

    Day2: 色の移動を実装

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello JavaScript!");
+console.log("今日も挫折せず続けててえらい");
 const colors = {
     1:"#ff0000",
     2:"#0000ff",
@@ -17,33 +17,52 @@ const stage =[
 
 /**現在の選択状態を入れる変数 */
 let selectedTube = null;
+let firstIndex = null;
+let secondIndex = null;
 
 const game = document.getElementById("game");
-for(let i = 0; i < 5; i++){
+
+
+function render(){
+    // tubeの中身をすべて削除
+    game.innerHTML = "";
+    // tubeの中身更新
+    for(let i = 0; i < 5; i++){
     const tube = document.createElement("div");
     tube.className = "tube";
+    tube.dataset.index = i;
 
     tube.addEventListener("click",function(){
-        if(selectedTube){
+        if(firstIndex === null){
+            selectedTube = tube;
+            tube.classList.add("selected");
+            firstIndex = Number(tube.dataset.index);
+            secondIndex = null;
+        }else{
             if(selectedTube === tube){
                 tube.classList.remove("selected");
                 selectedTube = null;
+                firstIndex = null;
             }else{
-                selectedTube.classList.remove("selected");
-                /**クリックされたtubeというHTML要素に、selectedというclassを追加した */
-                tube.classList.add("selected");
-                selectedTube = tube;
+                /**異なる2色を選択した場合 */
+                secondIndex = Number(tube.dataset.index);
+                console.log(
+                    "移動元", firstIndex,
+                    "移動先" , secondIndex
+                );
+                const moveColor = stage[firstIndex].shift();
+                stage[secondIndex].unshift(moveColor);
+                selectedTube = null;
+                firstIndex = null;
+                secondIndex = null;
+                render();
             }
-        }else{
-            selectedTube = tube;
-            tube.classList.add("selected");
         }
-        console.log(selectedTube);
     })
 
     game.append(tube);
 
-    for(let j=0; j<4; j++){
+    for(let j = 0; j < stage[i].length; j++){
         const liquid = document.createElement("div");
         liquid.className = "liquid";
         liquid.style.backgroundColor = colors[stage[i][j]];
@@ -51,3 +70,6 @@ for(let i = 0; i < 5; i++){
     }
     }
 
+}
+
+render();
